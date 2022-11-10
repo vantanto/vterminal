1e3ace507c780273454509e8ce3035e64bd0cb23
vantanto <56611021+vantanto@users.noreply.github.com>
2022-11-10 15:00:04 +0700

    fix: text color style.css

--- assets/js/bin/api_commands.js
+++ assets/js/bin/api_commands.js
@@ -10,7 +10,7 @@ export const projects = async (args) => {
   return projects
     .map(
       (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
+        `${repo.name} - <a class="text-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
     )
     .join('\n');
 };
